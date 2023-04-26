d0dead40345db85ffccbc46ef4941aeee99b664b
KDK <kryukovakatya@gmail.com>
2023-04-26 13:45:08 +0300

    init

--- test_package/test_func.py
+++ test_package/test_func.py
@@ -1,5 +1,13 @@
-def test_f(a, b):
+def plus(a, b):
     return a+b
 
+def minus(a, b):
+    return a - b
+
+def mul(a, b):
+    return a*b
+
 if __name__ == '__main__':
-    print(test_f(2,4))
+    print(plus(2,4))
+    print(minus(5, 2))
+    print(mul(3, 3))
